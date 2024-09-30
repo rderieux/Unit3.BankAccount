7330bc5f0d521f91ef0455b908480c5cb187ee88
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-30 11:14:40 -0600

    Add a transactionsReducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // TODO: Configure the store to use the reducer from the transactions slice.
-export const store = configureStore();
+export const store = configureStore({
+  reducer: {
+    transactions: transactionsReducer,
+  },
+});
