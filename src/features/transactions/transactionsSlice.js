7b57bea68fd5be9ecc45255b91ab35722bfc47e5
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-30 11:10:16 -0600

    Add deposit and transfer slices to transactionsSlice

--- src/features/transactions/transactionsSlice.js
+++ src/features/transactions/transactionsSlice.js
@@ -11,7 +11,11 @@ import { createSlice } from "@reduxjs/toolkit";
 // TODO: Set initial state to have a balance of 0 and an empty array of transactions.
 
 /** @type {{balance: number, history: Transaction[]}} */
-const initialState = {};
+const initialState = {
+  amount: 0,
+  balance: 0,
+  transactions: [],
+};
 
 /* TODO
 Add two reducers  to the transactions slice: "deposit" and "transfer".
@@ -31,6 +35,22 @@ const transactionsSlice = createSlice({
       state.balance -= payload.amount;
       state.history.push({
         type: "withdrawal",
+        amount: payload.amount,
+        balance: state.balance,
+      });
+    },
+    deposit: (state, { payload }) => {
+      state.balance += payload.amount;
+      state.history.push({
+        type: "deposit",
+        amount: payload.amount,
+        balance: state.balance,
+      });
+    },
+    transfer: (state, { payload }) => {
+      state.balance -= payload.amount;
+      state.history.push({
+        type: "transfer",
         amount: payload.amount,
         balance: state.balance,
       });
